31792356b423be868a830445fb0fc9e9fd547a14
sankes1989 <98402326+sankes1989@users.noreply.github.com>
2024-02-06 22:02:55 +0300

    Update index.php

--- Сортировка массива по значениям/index.php
+++ Сортировка массива по значениям/index.php
@@ -1,1 +1,20 @@
+<?
+function array_orderby()
+{
+    $args = func_get_args();
+    $data = array_shift($args);
+    foreach ($args as $n => $field) {
+        if (is_string($field)) {
+            $tmp = array();
+            foreach ($data as $key => $row)
+                $tmp[$key] = $row[$field];
+            $args[$n] = $tmp;
+            }
+    }
+    $args[] = &$data;
+    call_user_func_array('array_multisort', $args);
+    return array_pop($args);
+}
 
+$sorted = array_orderby($data, 'volume', SORT_DESC, 'edition', SORT_ASC);
+?>
